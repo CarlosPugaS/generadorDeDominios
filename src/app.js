6b9885939f8d1ffe2184061cb44eef2a4758e9ed
CarlosPuga <c.puga26@gmail.com>
2024-04-29 18:22:40 -0400

    Generador de Nombres de Dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let a = 0; a < pronoun.length; a++) {
+  for (let b = 0; b < adj.length; b++) {
+    for (let c = 0; c < noun.length; c++) {
+      let combination = pronoun[a] + adj[b] + noun[c] +'.com';
+      console.log(combination);
+    }
+  }
+}
